4c1e81821938e6105fb9717f890f27ffbfe90e0b
psinus <psinus.me@gmail.com>
2024-09-04 16:08:16 +0300

    03.09.24

--- godeeper/main.js
+++ godeeper/main.js
@@ -1,16 +1,6 @@
-// <!DOCTYPE html>
-// <html>
-//     <head>
-//         <meta charset="utf-8" />
-//         <title>Go deeper</title>
-//         <link rel="stylesheet" href="css/game.css" type="text/css" media="screen">
-//     </head>
-//     <body>
-//         <div id="dpi" style="height: 1in; width: 1in; left:100%; position: fixed; top: 100%; " ></div>
-//         <canvas id="canvas"> </canvas>
-//         <script src="main.js"></script>
-//     </body>
-// </html>
+
+
+
 let divLog = document.getElementById('log');
 let dpi_x = document.getElementById('dpi').offsetWidth;
 let dpi_a = window.devicePixelRatio;
@@ -25,6 +15,8 @@ document.fonts.add(font1);
 let images = new Map();
 let canvas = document.getElementById('canvas');
 let ctx = canvas.getContext('2d');
+
+
 let notScaledCanvas = document.createElement('canvas');
 let notScaledCtx = notScaledCanvas.getContext('2d');
 
@@ -75,12 +67,6 @@ function renderPresent() {
         ctx.imageSmoothingEnabled = false;
     }
 }
-
-let request = new XMLHttpRequest();
-request.open("GET", "game.wasm");
-request.responseType = 'arraybuffer';
-request.send();
-let countAwait = 0;
 
 class WsEvent {
     constructor() {
@@ -174,29 +160,43 @@ class WsEvent {
         this.clear();
     }
 }
+
+function startMobile(){
+
+    fullScreen();
+    canvas.removeEventListener('touchstart');
+    
+    canvas.addEventListener('touchstart', (event)=>{
+        wsEvent.kind = 1;
+        wsEvent.crdx = event.changedTouches[0].screenX - canvas.offsetLeft;
+        wsEvent.crdy = event.changedTouches[0].clientY - canvas.offsetTop;
+        wsEvent.btnN = event.button;
+    
+    });
+    canvas.addEventListener('touchend', (event)=>{
+        wsEvent.kind = 2;
+        wsEvent.crdx = event.changedTouches[0].screenX - canvas.offsetLeft;
+        wsEvent.crdy = event.changedTouches[0].clientY - canvas.offsetTop;
+        wsEvent.btnN = event.button;
+    
+    });
+    canvas.addEventListener('touchmove', (event)=>{
+        wsEvent.kind = 3;
+        wsEvent.crdx = event.changedTouches[0].screenX - canvas.offsetLeft;
+        wsEvent.crdy = event.changedTouches[0].clientY - canvas.offsetTop;
+    
+    });
+}
+
+
 let wsEvent = new WsEvent();
 {
     if('ontouchstart' in document.documentElement){
         canvas.addEventListener('touchstart', (event)=>{
-            wsEvent.kind = 1;
-            wsEvent.crdx = event.changedTouches[0].screenX - canvas.offsetLeft;
-            wsEvent.crdy = event.changedTouches[0].clientY - canvas.offsetTop;
-            wsEvent.btnN = event.button;
-        
+            startMobile();            
         });
-        canvas.addEventListener('touchend', (event)=>{
-            wsEvent.kind = 2;
-            wsEvent.crdx = event.changedTouches[0].screenX - canvas.offsetLeft;
-            wsEvent.crdy = event.changedTouches[0].clientY - canvas.offsetTop;
-            wsEvent.btnN = event.button;
-        
-        });
-        canvas.addEventListener('touchmove', (event)=>{
-            wsEvent.kind = 3;
-            wsEvent.crdx = event.changedTouches[0].screenX - canvas.offsetLeft;
-            wsEvent.crdy = event.changedTouches[0].clientY - canvas.offsetTop;
-        
-        });
+        ctx.font = '48px serif';
+        ctx.fillText("Touch to Start");
     }else{
         canvas.addEventListener('mousedown', (event) => {
             wsEvent.kind = 1;
@@ -225,189 +225,200 @@ let wsEvent = new WsEvent();
         // console.log(event.code);
     });
 }
-request.onload = function () {
-    let bytes = request.response;
-    WebAssembly.instantiate(bytes, { env: {
-            JsDraw: function (idImage, sx, sy, sw, sh, dx, dy, dw, dh) {
-                if (notScaledCtx != null) {
-                    notScaledCtx.drawImage(images[idImage], sx, sy, sw, sh, dx, dy, dw, dh);
-                }
-            },
-            JsPrint: function (s) {
-                let text = new TextDecoder().decode(data);
-                divLog.innerHTML = text;
-                console.log(text);
-            },
-            JsAlloc: function (size) {
-                data = new Uint8Array(size);
-            },
-            JsSetData: function (idx, val) {
-                data[idx] = val;
-            },
-            JsSetDrawColor: function (r, g, b, a) {
-                if (ctx != null) {
-                    ctx.fillStyle = "#000000";
-                }
-            },
-            JsClearCanvas: function () {
-                if (ctx != null) {
-                    ctx.fillStyle = "#000000";
-                    ctx.fillRect(0, 0, canvas.width, canvas.height);
-                }
-            },
-            JsImageFromFile: function (idImage) {
-                let path = new TextDecoder().decode(data);
-                let image = new Image();
-                image.src = path;
-                images[idImage] = image;
-                countAwait += 1;
-                image.onload = function () {
-                    countAwait -= 1;
-                };
-            },
-            JsSetImageAlpha: function (idImage, alpha) {
-                let image = images[idImage].getContext('2d');
-                image.globalAlpha = alpha;
-            },
-            JsDestroyImage: function (idImage) {
-                images.delete(idImage);
-            },
-            JsQueryImage: function (idImage, query) {
-                let image = images[idImage];
-                let result = 0;
-                if (query == 0) {
-                    result = image.width;
-                }
-                else if (query == 1) {
-                    result = image.height;
-                }
-                return result;
-            },
-            JsImageEmpty: function (idImage, w, h) {
-                let image = document.createElement('canvas');
-                image.width = w;
-                image.height = h;
-                images[idImage] = image;
-            },
-            JsFillRect: function (idImage, dx, dy, dw, dh, r, g, b, a) {
-                let image = images[idImage].getContext('2d');
-                image.fillStyle = `rgb(${r}, ${g}, ${b})`;
-                image.globalAlpha = a;
-                image.fillRect(dx, dy, dw, dh);
-            },
-            JsImageDraw: function (idSrcImage, idDstImage, sx, sy, sw, sh, dx, dy, dw, dh) {
-                let srcImage = images[idSrcImage];
-                let dstImage = images[idDstImage].getContext('2d');
-                dstImage.drawImage(srcImage, sx, sy, sw, sh, dx, dy, dw, dh);
-            },
-            JsFontFromFile: function (idFont, size) { },
-            JsMeasureText: function (idFont, query) {
-                let result = 0;
-                if (notScaledCtx != null) {
-                    let text = new TextDecoder().decode(data);
-                    let n = notScaledCtx.measureText(text);
-                    if (query == 0) {
-                        result = n.width;
-                    }
-                    else if (query == 1) {
-                        result = 12;
-                    }
-                }
-                return result;
-            },
-            JsDrawText: function (idFont, idImage, r, g, b, a) {
-                let text = new TextDecoder().decode(data);
-                let image = images[idImage].getContext('2d');
-                image.font = "12px font1";
-                image.textAlign = "left";
-                image.textBaseline = "top";
-                image.globalAlpha = a;
-                image.fillStyle = `rgb(${r}, ${g}, ${b})`;
-                image.fillText(text, 0, 0);
-            },
-            JsGetMillitimeStamp: function () {
-                let n = Date.now();
-                console.log(n);
-                return 123456;
-            },
-        } }).then(result => {
-        let exp = result.instance.exports;
-        const encoder = new TextEncoder();
-        const str = encoder.encode("Hello");
-        // exp.WsAlloc(3);
-        // exp.WsSetData(0, 10);
-        // exp.WsSetData(1, 100);
-        // exp.WsSetData(2, 999);
-        function Main() {
-            document.oncontextmenu = () => {
-                return false;
-            };
-            ctx.imageSmoothingEnabled = false;
-            notScaledCtx.imageSmoothingEnabled = false;   
-         const maxDimX = 23;
-            const maxDimY = 17;
-            divLog.innerHTML += " : " + window.screen.width.toString() + ", " + window.screen.height.toString() + " : "; 
-                
-            let sqr = 32 * scale;
-            let dimX = Math.floor(window.innerWidth / sqr);
-            if (dimX > maxDimX) {
-                dimX = maxDimX;
-            }else if(dimX < 13){dimX = 13;}
-            
-            let dimY = Math.floor(window.innerHeight / sqr);
-            if (dimY > maxDimY) {
-                dimY = maxDimY;
-            }else if(dimY < 7){dimY = 7;}
-            divLog.innerHTML += dimX.toString() + ", " + dimY.toString();
-            let borderW = 3 * scale;
-            const FRAME_RATE = 50;
-            let width = (dimX * sqr + (2 * borderW));
-            let height = (dimY * sqr + (2 * borderW));
-            canvas.width = width;
-            canvas.height = height;
-            notScaledCanvas.width = dimX * 32 + (2 * 3);
-            notScaledCanvas.height = dimY * 32 + (2 * 3);
 
-            
-            let requestAnimFrame = (function () {
-                return window.requestAnimationFrame ||
-                    window.webkitRequestAnimationFrame ||
-                    window.mozRequestAnimationFrame ||
-                    window.onRequestAnimationFrame ||
-                    window.msRequestAnimationFrame ||
-                    function (callback, element) {
-                        window.setTimeout(callback, 1000 / FRAME_RATE);
-                    };
-            })();
-            // let client = new WebSocket('http://127.0.0.1:9224');
-            // client.addEventListener
-            // client.onopen = function(){
-            //     client.send('Hello');
-            // }
-            // client.onmessage = function(ev: MessageEvent){
-            //     console.log(ev.data);
-            // }
-            exp.WsInitStage1(notScaledCanvas.width, notScaledCanvas.height);
-            let lastTime = 0;
-            notScaledCtx.font = "12px font1";
-            notScaledCtx.textAlign = "left";
-            notScaledCtx.textBaseline = "top";
-            center();
-            let GameCycle = () => {
-                if (countAwait == 0) {
-                    exp.WsInitStage2();
+
+
+function startGame(){
+    let request = new XMLHttpRequest();
+    request.open("GET", "game.wasm");
+    request.responseType = 'arraybuffer';
+    request.send();
+    let countAwait = 0;
+
+    request.onload = function () {
+        let bytes = request.response;
+        WebAssembly.instantiate(bytes, { env: {
+                JsDraw: function (idImage, sx, sy, sw, sh, dx, dy, dw, dh) {
+                    if (notScaledCtx != null) {
+                        notScaledCtx.drawImage(images[idImage], sx, sy, sw, sh, dx, dy, dw, dh);
+                    }
+                },
+                JsPrint: function (s) {
+                    let text = new TextDecoder().decode(data);
+                    divLog.innerHTML = text;
+                    console.log(text);
+                },
+                JsAlloc: function (size) {
+                    data = new Uint8Array(size);
+                },
+                JsSetData: function (idx, val) {
+                    data[idx] = val;
+                },
+                JsSetDrawColor: function (r, g, b, a) {
                     if (ctx != null) {
                         ctx.fillStyle = "#000000";
-                        ctx.fillRect(0, 0, width, height);
-                        wsEvent.send(exp);
-                        exp.WsUpdate();
-                        renderPresent();
                     }
-                }
-                requestAnimFrame(GameCycle);
-            };
-            GameCycle();
-        }
-        Main();
-    });
-};
+                },
+                JsClearCanvas: function () {
+                    if (ctx != null) {
+                        ctx.fillStyle = "#000000";
+                        ctx.fillRect(0, 0, canvas.width, canvas.height);
+                    }
+                },
+                JsImageFromFile: function (idImage) {
+                    let path = new TextDecoder().decode(data);
+                    let image = new Image();
+                    image.src = path;
+                    images[idImage] = image;
+                    countAwait += 1;
+                    image.onload = function () {
+                        countAwait -= 1;
+                    };
+                },
+                JsSetImageAlpha: function (idImage, alpha) {
+                    let image = images[idImage].getContext('2d');
+                    image.globalAlpha = alpha;
+                },
+                JsDestroyImage: function (idImage) {
+                    images.delete(idImage);
+                },
+                JsQueryImage: function (idImage, query) {
+                    let image = images[idImage];
+                    let result = 0;
+                    if (query == 0) {
+                        result = image.width;
+                    }
+                    else if (query == 1) {
+                        result = image.height;
+                    }
+                    return result;
+                },
+                JsImageEmpty: function (idImage, w, h) {
+                    let image = document.createElement('canvas');
+                    image.width = w;
+                    image.height = h;
+                    images[idImage] = image;
+                },
+                JsFillRect: function (idImage, dx, dy, dw, dh, r, g, b, a) {
+                    let image = images[idImage].getContext('2d');
+                    image.fillStyle = `rgb(${r}, ${g}, ${b})`;
+                    image.globalAlpha = a;
+                    image.fillRect(dx, dy, dw, dh);
+                },
+                JsImageDraw: function (idSrcImage, idDstImage, sx, sy, sw, sh, dx, dy, dw, dh) {
+                    let srcImage = images[idSrcImage];
+                    let dstImage = images[idDstImage].getContext('2d');
+                    dstImage.drawImage(srcImage, sx, sy, sw, sh, dx, dy, dw, dh);
+                },
+                JsFontFromFile: function (idFont, size) { },
+                JsMeasureText: function (idFont, query) {
+                    let result = 0;
+                    if (notScaledCtx != null) {
+                        let text = new TextDecoder().decode(data);
+                        let n = notScaledCtx.measureText(text);
+                        if (query == 0) {
+                            result = n.width;
+                        }
+                        else if (query == 1) {
+                            result = 12;
+                        }
+                    }
+                    return result;
+                },
+                JsDrawText: function (idFont, idImage, r, g, b, a) {
+                    let text = new TextDecoder().decode(data);
+                    let image = images[idImage].getContext('2d');
+                    image.font = "12px font1";
+                    image.textAlign = "left";
+                    image.textBaseline = "top";
+                    image.globalAlpha = a;
+                    image.fillStyle = `rgb(${r}, ${g}, ${b})`;
+                    image.fillText(text, 0, 0);
+                },
+                JsGetMillitimeStamp: function () {
+                    let n = Date.now();
+                    console.log(n);
+                    return 123456;
+                },
+            } }).then(result => {
+            let exp = result.instance.exports;
+            const encoder = new TextEncoder();
+            const str = encoder.encode("Hello");
+            // exp.WsAlloc(3);
+            // exp.WsSetData(0, 10);
+            // exp.WsSetData(1, 100);
+            // exp.WsSetData(2, 999);
+            function Main() {
+                document.oncontextmenu = () => {
+                    return false;
+                };
+                ctx.imageSmoothingEnabled = false;
+                notScaledCtx.imageSmoothingEnabled = false;   
+             const maxDimX = 23;
+                const maxDimY = 17;
+                divLog.innerHTML += " : " + window.screen.width.toString() + ", " + window.screen.height.toString() + " : "; 
+                
+                let sqr = 32 * scale;
+                let dimX = Math.floor(window.innerWidth / sqr);
+                if (dimX > maxDimX) {
+                    dimX = maxDimX;
+                }else if(dimX < 13){dimX = 13;}
+            
+                let dimY = Math.floor(window.innerHeight / sqr);
+                if (dimY > maxDimY) {
+                    dimY = maxDimY;
+                }else if(dimY < 7){dimY = 7;}
+                divLog.innerHTML += dimX.toString() + ", " + dimY.toString();
+                let borderW = 3 * scale;
+                const FRAME_RATE = 50;
+                let width = (dimX * sqr + (2 * borderW));
+                let height = (dimY * sqr + (2 * borderW));
+                canvas.width = width;
+                canvas.height = height;
+                notScaledCanvas.width = dimX * 32 + (2 * 3);
+                notScaledCanvas.height = dimY * 32 + (2 * 3);
+
+            
+                let requestAnimFrame = (function () {
+                    return window.requestAnimationFrame ||
+                        window.webkitRequestAnimationFrame ||
+                        window.mozRequestAnimationFrame ||
+                        window.onRequestAnimationFrame ||
+                        window.msRequestAnimationFrame ||
+                        function (callback, element) {
+                            window.setTimeout(callback, 1000 / FRAME_RATE);
+                        };
+                })();
+                // let client = new WebSocket('http://127.0.0.1:9224');
+                // client.addEventListener
+                // client.onopen = function(){
+                //     client.send('Hello');
+                // }
+                // client.onmessage = function(ev: MessageEvent){
+                //     console.log(ev.data);
+                // }
+                exp.WsInitStage1(notScaledCanvas.width, notScaledCanvas.height);
+                let lastTime = 0;
+                notScaledCtx.font = "12px font1";
+                notScaledCtx.textAlign = "left";
+                notScaledCtx.textBaseline = "top";
+                center();
+                let GameCycle = () => {
+                    if (countAwait == 0) {
+                        exp.WsInitStage2();
+                        if (ctx != null) {
+                            ctx.fillStyle = "#000000";
+                            ctx.fillRect(0, 0, width, height);
+                            wsEvent.send(exp);
+                            exp.WsUpdate();
+                            renderPresent();
+                        }
+                    }
+                    requestAnimFrame(GameCycle);
+                };
+                GameCycle();
+            }
+            Main();
+        });
+    };
+}
